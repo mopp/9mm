0e19c530246d81fbbcec6cf44de1ba7438d8cd2f
mopp <hello@mopp.jp>
2019-05-12 22:13:52 +0900

    足し算と引き算を追加

--- 9mm.c
+++ 9mm.c
@@ -8,10 +8,30 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    char* p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("  ret\n");
     return 0;
 }
