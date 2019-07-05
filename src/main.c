30ca0711bf79bd500eab4192214c91fe26bcd3cd
mopp <hello@mopp.jp>
2019-07-05 23:34:38 +0900

    feat: read c source from file

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include "9mm.h"
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 
+static char const* read_file(char const*);
 static char const* input;
+static char const* filename;
 
 int main(int argc, char const* const* argv)
 {
-    if (argc != 2) {
+    if (argc < 2) {
         error("引数の個数が正しくありません");
         return 1;
     }
@@ -15,9 +18,13 @@ int main(int argc, char const* const* argv)
     if (strncmp("-test", argv[1], 5) == 0) {
         runtest();
         return 0;
+    } else if (strncmp("-str", argv[1], 4) == 0) {
+        input = argv[2];
+    } else {
+        filename = argv[1];
+        input = read_file(filename);
     }
 
-    input = argv[1];
     Vector const* tokens = tokenize(input);
     Node const* const* code = program(tokens);
 
@@ -62,8 +69,33 @@ int main(int argc, char const* const* argv)
 // Output an error for user and exit.
 _Noreturn void error_at(char const* loc, char const* msg)
 {
-    int pos = loc - input;
-    fprintf(stderr, "\e[1m%s\n%*s^ %s\n\e[0m", input, pos, "", msg);
+    // locが含まれている行の開始地点と終了地点を取得
+    char const* line = loc;
+    while (input < line && line[-1] != '\n') {
+        line--;
+    }
+
+    char const* end = loc;
+    while (*end != '\n') {
+        end++;
+    }
+
+    // 見つかった行が全体の何行目なのかを調べる
+    size_t line_num = 1;
+    for (char const* p = input; p < line; p++) {
+        if (*p == '\n') {
+            line_num++;
+        }
+    }
+
+    // 見つかった行を、ファイル名と行番号と一緒に表示
+    size_t indent = fprintf(stderr, "%s:%zd: ", filename, line_num);
+    fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+    // エラー箇所を"^"で指し示して、エラーメッセージを表示
+    int pos = loc - line + indent;
+    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "^ %s\n", msg);
     exit(1);
 }
 
@@ -76,4 +108,37 @@ void _log(char const* level, const char* file, const char* func, size_t line, ch
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     va_end(ap);
+}
+
+// 指定されたファイルの内容を返す
+static char const* read_file(char const* path)
+{
+    // ファイルを開く
+    FILE* fp = fopen(path, "r");
+    if (fp == NULL) {
+        error("cannot open %s: %s", path, strerror(errno));
+    }
+
+    // ファイルの長さを調べる
+    if (fseek(fp, 0, SEEK_END) == -1) {
+        error("%s: fseek: %s", path, strerror(errno));
+    }
+    size_t size = ftell(fp);
+    if (fseek(fp, 0, SEEK_SET) == -1) {
+        error("%s: fseek: %s", path, strerror(errno));
+    }
+
+    // ファイル内容を読み込む
+    char* buf = calloc(1, size + 2);
+    fread(buf, size, 1, fp);
+
+    // ファイルが必ず"\n\0"で終わっているようにする
+    if (size == 0 || buf[size - 1] != '\n') {
+        buf[size++] = '\n';
+    }
+    buf[size] = '\0';
+
+    fclose(fp);
+
+    return buf;
 }
