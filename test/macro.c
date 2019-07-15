d664ee1db70b79c6978e1136f732afb0cbe05c33
mopp <hello@mopp.jp>
2019-07-15 13:32:55 +0900

    test: add macro test

---
+++ test/macro.c
@@ -0,0 +1,46 @@
+// head
+#ifndef MOPP
+// ok
+#endif
+
+#ifndef MOPP
+// ok
+#else
+// error
+#endif
+
+#ifdef MOPP
+// error
+#endif
+
+#ifdef MOPP
+// error
+#else
+// ok
+#endif
+
+#define MOPP
+
+#ifndef MOPP
+// error
+#endif
+
+#ifndef MOPP
+// error
+#else
+// ok
+#endif
+
+#ifdef MOPP
+// ok
+#endif
+
+#ifdef MOPP
+// ok
+#else
+// error
+#endif
+
+int main(void) {
+    return 0;
+}
